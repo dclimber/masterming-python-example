1b384fc1b2124c07daa081ee966e18cbd8f456cb
Dastan <dastan.abdrakhmanov@orbem.ai>
2024-08-27 14:27:12 +0200

    tests: Add first test

---
+++ object-oriented-event-sourcing/tests/test.py
@@ -0,0 +1,33 @@
+import unittest
+import uuid
+
+from mastermind.domain import commands, events, executor, value
+
+
+def any_game_id() -> value.GameId:
+    return value.GameId(str(uuid.uuid4()))
+
+
+class TestGameExamples(unittest.TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.game_id = any_game_id()
+        self.secret = value.Code("Red", "Green", "Blue", "Yellow")
+        self.total_attempts = 12
+        self.available_pegs = value.set_of_pegs(
+            "Red", "Green", "Blue", "Yellow", "Purple", "Pink"
+        )
+
+    def test_it_starts_the_game(self) -> None:
+        expected = [
+            events.GameStarted(
+                self.game_id, self.secret, self.total_attempts, self.available_pegs
+            )
+        ]
+        command = commands.JoinGame(
+            self.game_id, self.secret, self.total_attempts, self.available_pegs
+        )
+
+        result = executor.execute(command)
+
+        self.assertEqual(result, expected)
